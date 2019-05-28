c3936779955ae082ce68f5a628fd9e15a5d0239d
XhstormR <10527522+XhstormR@users.noreply.github.com>
2019-05-28 16:04:14 +0800

    Signed-off-by: XhstormR <10527522+XhstormR@users.noreply.github.com>

--- app/src/main/java/com/example/leo/myapplication/Callback2.kt
+++ app/src/main/java/com/example/leo/myapplication/Callback2.kt
@@ -2,12 +2,26 @@ package com.example.leo.myapplication
 
 import android.util.Log
 import de.robv.android.xposed.XC_MethodHook
+import java.util.*
 
 object Callback2 : XC_MethodHook() {
     override fun beforeHookedMethod(param: MethodHookParam) {
-        Thread.dumpStack()
-        Log.i(Const.TAG, param.method.toString())
-        param.args.forEach { Log.i(Const.TAG, it.toString()) }
+        trace(param)
+        val args = param.args?.let { Arrays.toString(it) } ?: "void"
+        Log.i(Const.TAG, "args: $args")
         Log.i(Const.TAG, "----")
+    }
+
+    override fun afterHookedMethod(param: MethodHookParam) {
+        trace(param)
+        val result = param.result?.toString() ?: "void"
+        Log.i(Const.TAG, "result: $result")
+        Log.i(Const.TAG, "----")
+    }
+
+    private fun trace(param: MethodHookParam) {
+        Log.i(Const.TAG, "Stack trace:")
+        Exception().stackTrace.forEach { Log.i(Const.TAG, "\tat $it") }
+        Log.i(Const.TAG, param.method.toString())
     }
 }
